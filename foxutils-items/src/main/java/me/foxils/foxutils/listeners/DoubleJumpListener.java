3216e91e9ea21be2cf6c464186461a162de3f012
notFoxils <voltrixz9514@gmail.com>
2024-11-16 19:02:57 -0500

    Prevent error on join with DoubleJumpListener

--- foxutils-items/src/main/java/me/foxils/foxutils/listeners/DoubleJumpListener.java
+++ foxutils-items/src/main/java/me/foxils/foxutils/listeners/DoubleJumpListener.java
@@ -1,43 +1,38 @@
 package me.foxils.foxutils.listeners;
 
-import de.unpixelt.armorchange.ArmorPlugin;
 import me.foxils.foxutils.registry.ItemRegistry;
 import me.foxils.foxutils.itemactions.DoubleJumpAction;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class DoubleJumpListener implements Listener {
-    
+
+    /*
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
         Player playerWhoJoined = playerJoinEvent.getPlayer();
 
         ArmorPlugin.reequipPlayer(playerWhoJoined);
-    }
+    }*/
 
     @EventHandler
     public void onDoubleJump(PlayerToggleFlightEvent toggleFlightEvent) {
-        Player player = toggleFlightEvent.getPlayer();
+        final Player player = toggleFlightEvent.getPlayer();
 
-        if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) {
-            return;
-        }
+        if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) return;
 
+        // Could interfere with other plugins and general flying for admins
+        // TODO: Find a proper way to do this
         toggleFlightEvent.setCancelled(true);
 
-        if(!(toggleFlightEvent.isFlying())) {
-            return;
-        }
+        if (!toggleFlightEvent.isFlying()) return;
 
         for (ItemStack item : player.getInventory().getContents()) {
-            if (!(ItemRegistry.getItemFromItemStack(item) instanceof DoubleJumpAction itemWithDoubleJumpAction)) {
-                continue;
-            }
+            if (!(ItemRegistry.getItemFromItemStack(item) instanceof DoubleJumpAction itemWithDoubleJumpAction)) continue;
 
             itemWithDoubleJumpAction.doubleJumpAction(toggleFlightEvent, item);
         }
