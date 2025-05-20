85956125c9c64e3dea368109b96a94c92563e438
notFoxils <voltrixz9514@gmail.com>
2025-05-20 17:10:55 -0400

    foxutils-config revamp

--- foxutils-config/src/main/java/me/foxils/foxutils/utilities/SectionManager.java
+++ foxutils-config/src/main/java/me/foxils/foxutils/utilities/SectionManager.java
@@ -6,17 +6,13 @@ import dev.dejvokep.boostedyaml.block.implementation.Section;
 
 public abstract class SectionManager {
 
-    protected final Section section;
+    private final Section section;
 
     public SectionManager(final @NotNull Section sectionToManage) {
         this.section = sectionToManage;
     }
 
-    public Section getSection() {
+    protected Section getSection() {
         return this.section;
-    }
-
-    public String getName() {
-        return getSection().getNameAsString();
     }
 }
